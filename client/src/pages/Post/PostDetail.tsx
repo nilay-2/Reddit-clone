d692467c4b7609957cfcc26adac91becaf7a43d0
Nilay Pophalkar <nilaypophalkar@gmail.com>
2024-03-11 19:59:51 +0530

    fixed the header sticky bug

--- client/src/pages/Post/PostDetail.tsx
+++ client/src/pages/Post/PostDetail.tsx
@@ -50,64 +50,66 @@ const PostDetail: React.FC = () => {
   }
 
   return (
-    <div className="text-white post-container max-w-4xl mx-auto flex flex-col gap-3 items-start py-5">
-      <button
-        onClick={() => {
-          dispatch(removeSelectedPost());
-          dispatch(reset());
-          navigate("/");
-        }}
-      >
-        <i className="bi bi-arrow-left-circle text-2xl"></i>
-      </button>
-      <PostElement post={postState.selectedPost} />
-      <div className="comment-box w-full">
-        <div className="p-2 flex w-full">
-          <TextareaAutosize
-            value={comment}
-            minRows={1}
-            className="w-full p-2 outline outline-stone-700 outline-1 rounded-md"
-            style={{ backgroundColor: "#242425" }}
-            placeholder="Add a comment"
-            onChange={textAreaOnChangeHandler}
-          />
-        </div>
-        <div className="flex justify-end">
-          <div className="btn-group flex gap-2 px-5">
-            <button
-              className="px-3 py-1 rounded-lg bg-red-600 text-white hover:bg-red-500 active:bg-red-600"
-              onClick={() => {
-                setComment("");
-              }}
-            >
-              cancel
-            </button>
-            <button
-              className={`px-3 py-1 rounded-lg text-white ${
-                comment ? "bg-blue-600" : "bg-blue-400"
-              } hover:bg-blue-500 active:bg-blue-600`}
-              disabled={comment ? false : true}
-              onClick={() => {
-                if (postState.selectedPost) {
-                  dispatch(
-                    createComment({
-                      userid: authState.id,
-                      postid: postState.selectedPost?.id,
-                      content: comment,
-                      replyto: null,
-                      username: authState.username,
-                    })
-                  );
+    <div style={{ height: "calc(100vh - 70px)" }} className="overflow-auto">
+      <div className="text-white post-container max-w-4xl mx-auto flex flex-col gap-3 items-start py-5">
+        <button
+          onClick={() => {
+            dispatch(removeSelectedPost());
+            dispatch(reset());
+            navigate("/");
+          }}
+        >
+          <i className="bi bi-arrow-left-circle text-2xl"></i>
+        </button>
+        <PostElement post={postState.selectedPost} />
+        <div className="comment-box w-full">
+          <div className="p-2 flex w-full">
+            <TextareaAutosize
+              value={comment}
+              minRows={1}
+              className="w-full p-2 outline outline-stone-700 outline-1 rounded-md"
+              style={{ backgroundColor: "#242425" }}
+              placeholder="Add a comment"
+              onChange={textAreaOnChangeHandler}
+            />
+          </div>
+          <div className="flex justify-end">
+            <div className="btn-group flex gap-2 px-5">
+              <button
+                className="px-3 py-1 rounded-lg bg-red-600 text-white hover:bg-red-500 active:bg-red-600"
+                onClick={() => {
                   setComment("");
-                }
-              }}
-            >
-              Comment
-            </button>
+                }}
+              >
+                cancel
+              </button>
+              <button
+                className={`px-3 py-1 rounded-lg text-white ${
+                  comment ? "bg-blue-600" : "bg-blue-400"
+                } hover:bg-blue-500 active:bg-blue-600`}
+                disabled={comment ? false : true}
+                onClick={() => {
+                  if (postState.selectedPost) {
+                    dispatch(
+                      createComment({
+                        userid: authState.id,
+                        postid: postState.selectedPost?.id,
+                        content: comment,
+                        replyto: null,
+                        username: authState.username,
+                      })
+                    );
+                    setComment("");
+                  }
+                }}
+              >
+                Comment
+              </button>
+            </div>
           </div>
         </div>
+        <CommentsContainer postId={postState.selectedPost.id} />
       </div>
-      <CommentsContainer postId={postState.selectedPost.id} />
     </div>
   );
 };
